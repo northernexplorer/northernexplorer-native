5182b6c8cebda13de332aaa02362683a68f6bc83
Shayne Thiessen <shayne@shaynethiessen.com>
2026-06-19 21:43:24 -0500

    Default Data

--- apps/migrate/src/migrations/index.ts
+++ apps/migrate/src/migrations/index.ts
@@ -1,6 +1,8 @@
 import { m20260619 } from './m20260619';
+import {m20260620} from "./m20260620";
 
 // Map your unique key names directly to their SQL statement arrays
 export const migrationsRegistry: Record<string, string[]> = {
-  m20260619,
+    m20260619,
+    m20260620,
 };
