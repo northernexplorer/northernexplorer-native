3d5f09f6d385803d2c0f8b0515d5e8eeac22666e
Shayne Thiessen <shayne@shaynethiessen.com>
2026-06-20 19:46:58 -0500

    Map

--- apps/native/src/pages/Map/Map.tsx
+++ apps/native/src/pages/Map/Map.tsx
@@ -1,7 +1,24 @@
-import {Text } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
+import { Map as NativeMap, Camera } from '@maplibre/maplibre-react-native';
+import {useLocation} from "~/state/hooks/location/useLocation";
 
 export function Map() {
-    // @maplibre/maplibre-react-native.
-    // react-map-gl/maplibre
-    return <Text>Map</Text>
+    const coords = useLocation();
+
+    if(!coords) return null;
+
+    return (
+        <View style={{width: '100%', height: '100%' }}>
+            <NativeMap
+                style={{width: '100%', height: '100%' }}
+                mapStyle="https://tiles.openfreemap.org/styles/bright"
+            >
+                <Camera
+                    zoom={10}
+                    center={[coords.lon, coords.lat]}
+                />
+            </NativeMap>
+        </View>
+    );
 }
