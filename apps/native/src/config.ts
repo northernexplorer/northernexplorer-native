f1f0c1f6977b19fc8ff5ba0893b7612699b326b1
jesse <jessewldner1@gmail.com>
2026-06-21 17:34:07 -0500

    removed sensitive data from env default

--- apps/native/src/config.ts
+++ apps/native/src/config.ts
@@ -1,3 +1,3 @@
 export const config = {
-    SERVER_URL: process.env.EXPO_PUBLIC_SERVER_URL
+    SERVER_URL: process.env.EXPO_PUBLIC_SERVER_URL 
 }
--- apps/server/src/config.ts
+++ apps/server/src/config.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 
-dotenv.config({path:'.env.default'});
+dotenv.config();
 
 export const config = {
     PORT: process.env.PORT ? parseInt(process.env.PORT, 10) : 5001,
