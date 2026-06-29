b0a3e06514f8c3952bb4ce34bf3177be186e724c
Shayne Thiessen <shayne@shaynethiessen.com>
2026-06-29 14:16:17 -0500

    Use API

--- apps/native/src/config.ts
+++ apps/native/src/config.ts
@@ -5,10 +5,10 @@ function getServerUrl(): string {
   const appUrl = process.env.EXPO_PUBLIC_SERVER_URL_APP?.trim();
 
   if (Platform.OS === 'web') {
-    return webUrl || 'https://northernexplorer.org';
+    return webUrl || 'https://api.northernexplorer.org';
   }
 
-  return appUrl || 'https://northernexplorer.org';
+  return appUrl || 'https://api.northernexplorer.org';
 }
 
 export const config = {
