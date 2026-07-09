4173d8600452ae36081f1cf5eaf7d79c79d861c1
Shayne Thiessen <shayne@shaynethiessen.com>
2026-07-09 11:59:48 -0500

    fix: css

--- apps/native/src/layout/Layout/Layout.tsx
+++ apps/native/src/layout/Layout/Layout.tsx
@@ -23,7 +23,7 @@ export function Layout({ Content, components, title, fullPage, home }: Props) {
     const theme = weather ? getWeatherTheme(weather.current.condition.code) : null;
 
     return (
-        <View style={{ flex: 1 }}>
+        <View style={{ flex: 1, width: '100%', maxWidth: '100%', minWidth: 0, overflow: 'hidden' }}>
             <Navigation />
 
             <ScrollView
