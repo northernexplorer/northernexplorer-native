cbf124077e437fe57161d897e9d0160a6b69f9d3
Shayne Thiessen <shayne@shaynethiessen.com>
2026-07-16 13:57:45 -0500

    Update useApiFetch.ts

--- apps/native/src/core/useApiFetch.ts
+++ apps/native/src/core/useApiFetch.ts
@@ -1,4 +1,4 @@
-import {useState, useEffect, useCallback} from 'react';
+import {useState, useCallback} from 'react';
 import {ROUTES, GetParams, GetResponse, NonEmptyCategory} from '@northernexplorer/types';
 import {apiClient} from '~/core/apiClient';
 import {useAuthentication} from '~/user/state/authentication/useAuthentication';
