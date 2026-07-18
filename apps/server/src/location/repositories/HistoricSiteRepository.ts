48a19e541739e37eb8b512fe5e4eae7b18e94325
void <107288822+trenbolonesw@users.noreply.github.com>
2026-07-17 22:41:25 -0500

    latest main updates merged

--- apps/server/src/location/repositories/HistoricSiteRepository.ts
+++ apps/server/src/location/repositories/HistoricSiteRepository.ts
@@ -1,3 +1,5 @@
+import {CountryType} from '../../../../../packages/types/src/location/Country';
+import {RegionType} from '../../../../../packages/types/src/location/Region';
 import {HistoricSite} from '../entities/HistoricSite';
 import {EntityRepository} from '@mikro-orm/postgresql';
 
@@ -10,30 +12,45 @@ interface HistoricSiteRawRow {
 	lon: string | number;
 	startDate: string | number;
 	endDate: string | number;
-	country: string;
-	region: string;
+	country: CountryType;
+	region: RegionType;
 	distanceMeters: number;
 }
 
 export class HistoricSiteRepository extends EntityRepository<HistoricSite> {
 	async getHistoricSiteDetails(id: number) {
-		const site = await this.findOne({id: Number(id)});
-		if (!site) throw new Error("We couldn't find the historic site you are looking for.");
+		const site = await this.findOne({id: Number(id)}, {populate: ['country.name', 'region.name']});
+
+		if (!site) throw new Error('Historic site not found.');
 
 		return site;
 	}
 
 	async getClosestHistoricSites(lat: number, lon: number, limit: number) {
 		const query = `
-            SELECT id, name, description, image, lat, lon, country, region,
+            SELECT id, name, description, image, lat, lon, country  , region ,
                    start_date as "startDate", end_date as "endDate", distance_meters as distanceMeters
             FROM (
-                     SELECT id, name, description, image, lat, lon, country, region, start_date, end_date,
+                     SELECT h.id, h.name, h.description, h.image, h.lat, h.lon, 
+					  json_build_object(
+                'id', c.id,
+                'name', c.name
+            ) as country,
+            json_build_object(
+                'id', r.id,
+          
+                'name', r.name
+            ) as region,
+					 h.start_date, h.end_date,
                             (6371000 * acos(
-                                cos(radians(?)) * cos(radians(lat)) * cos(radians(lon) - radians(?)) +
-                                sin(radians(?)) * sin(radians(lat))
-                                       )) AS distance_meters
-                     FROM historic_site
+                                cos(radians(?)) * cos(radians(h.lat)) * cos(radians(h.lon) - radians(?)) +
+                                sin(radians(?)) * sin(radians(h.lat))
+                              )) AS distance_meters
+                     FROM historic_site h
+					 JOIN country c
+					 ON h.country_id = c.id
+					 JOIN region r
+					 ON h.region_id = r.id
                  ) AS spatial_search
             ORDER BY distanceMeters ASC
                 LIMIT ?
@@ -46,8 +63,8 @@ export class HistoricSiteRepository extends EntityRepository<HistoricSite> {
 			name: site.name,
 			description: site.description,
 			image: site.image,
-			country: site.country,
-			region: site.region,
+			country: site.country.name,
+			region: site.region.name,
 			lat: Number(site.lat),
 			lon: Number(site.lon),
 			startDate: site.startDate ? Number(site.startDate) : null,
