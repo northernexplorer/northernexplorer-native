c678a170073b0eb44fbc21c5d2d70daa44123625
Shayne Thiessen <shayne@shaynethiessen.com>
2026-07-19 09:38:33 -0500

    Add persist to basecontroller

--- apps/server/src/core/BaseController.ts
+++ apps/server/src/core/BaseController.ts
@@ -1,10 +1,16 @@
+import {AnyEntity} from '@mikro-orm/core';
 import {Repositories} from './repositories';
 
 export abstract class BaseController {
 	constructor(protected repos: Repositories) {}
 
-	protected async flush(): Promise<void> {
+	protected async flush() {
 		const em = this.repos.user.getEntityManager();
 		await em.flush();
+	}
+
+	protected persist(entity: AnyEntity) {
+		const em = this.repos.user.getEntityManager();
+		em.persist(entity);
 	}
 }
