8d2f6412dc9ad7354b27a457af172088872af32b
Shayne Thiessen <shayne@shaynethiessen.com>
2026-08-06 10:49:34 -0500

    Improve compass calibration Message

--- apps/native/src/app/location/compass.tsx
+++ apps/native/src/app/location/compass.tsx
@@ -2,5 +2,5 @@ import {Layout} from '~/layout/Layout';
 import {Compass} from '~/location/Compass';
 
 export default function () {
-	return <Layout Content={Compass} />;
+	return <Layout Content={Compass} showOffline />;
 }
