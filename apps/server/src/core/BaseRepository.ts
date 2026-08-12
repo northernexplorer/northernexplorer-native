91df119b76536271eb63a4949860a7fb5fdb7db8
Shayne Thiessen <shayne@shaynethiessen.com>
2026-08-11 19:02:45 -0500

    Update BaseRepository.ts

--- apps/server/src/core/BaseRepository.ts
+++ apps/server/src/core/BaseRepository.ts
@@ -1,4 +1,4 @@
-import {EntityRepository, EntityManager, AnyEntity, EntityData} from '@mikro-orm/core';
+import {EntityRepository, EntityManager, AnyEntity} from '@mikro-orm/core';
 
 // @ts-expect-error - Intentionally narrowing visibility from public to private
 export abstract class BaseRepository<T extends object> extends EntityRepository<T> {
@@ -9,7 +9,8 @@ export abstract class BaseRepository<T extends object> extends EntityRepository<
 		return this;
 	}
 
-	async execute<R = EntityData<T>>(query: string, params: unknown[] = []): Promise<R> {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+	async execute<R = Record<string, any>[]>(query: string, params: any[] = []): Promise<R> {
 		return this.em.getConnection().execute<R>(query, params);
 	}
 }
