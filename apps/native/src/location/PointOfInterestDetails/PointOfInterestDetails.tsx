1ffc3370640b561f7ed8e68b513328e810f3d5dc
Shayne Thiessen <shayne@shaynethiessen.com>
2026-08-21 13:47:10 -0500

    fix: poi

--- apps/native/src/location/PointOfInterestDetails/PointOfInterestDetails.tsx
+++ apps/native/src/location/PointOfInterestDetails/PointOfInterestDetails.tsx
@@ -8,6 +8,7 @@ import {styles} from '~/location/PointOfInterestDetails/styles';
 import {config} from '~/config';
 import {useApiFetch} from '~/core/useApiFetch';
 import {useAuthentication} from '~/user/state/authentication/useAuthentication';
+import {Map} from '~/location/PointOfInterestDetails/components/Map';
 
 export function PointOfInterestDetails() {
 	const {id} = useLocalSearchParams<{id: string}>();
@@ -18,7 +19,12 @@ export function PointOfInterestDetails() {
 
 	return (
 		<View>
-			<Image source={{uri: getUrl({path: data.image, serverUrl: config.SERVER_URL})}} style={styles.banner} />
+			<View style={styles.bannerContainer}>
+				<Image source={{uri: getUrl({path: data.image, serverUrl: config.SERVER_URL})}} style={styles.banner} />
+				<View style={styles.mapCard}>
+					<Map site={data} />
+				</View>
+			</View>
 			<View style={styles.content}>
 				<View style={styles.headerRow}>
 					<Text style={styles.breadcrumbs}>
