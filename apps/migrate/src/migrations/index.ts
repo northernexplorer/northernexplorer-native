41248ab3d1a7b9c8c46bd7d31d66edd984feddb9
Shayne Thiessen <shayne@shaynethiessen.com>
2026-09-10 16:28:50 -0500

    New Migration Structure (#313)

--- apps/migrate/src/migrations/index.ts
+++ apps/migrate/src/migrations/index.ts
@@ -1,103 +1,11 @@
-import {m20260619} from './m20260619';
-import {m20260620} from './m20260620';
-import {m20260622} from './m20260622';
-import {m20260703} from './m20260703';
-import {m20260703b} from './m20260703b';
-import {m20260703c} from './m20260703c';
-import {m20260705} from './m20260705';
-import {m20260705c} from './m20260705c';
-import {m20260706} from './m20260706';
-import {m20260707} from './m20260707';
-import {m20260709} from './m20260709';
-import {m20260708} from './m20260708';
-import {m20260710} from './m20260710';
-import {m20260710b} from './m20260710b';
-import {m20260710bc} from './m20260710c';
-import {m20260710d} from './m20260710d';
-import {m20260710e} from './m20260710e';
-import {m20260714f} from './m20260714f';
-import {m20260716} from './m20260716';
-import {m20260717} from './m20260717';
-import {m20260718} from './m20260718';
-import {m20260718a} from './m20260718a';
-import {m20260718b} from './m20260718b';
-import {m20260718c} from './m20260718c';
-import {m20260718d} from './m20260718d';
-import {m20260718e} from './m20260718e';
-import {m20260718f} from './m20260718f';
-import {m20260718g} from './m20260718g';
-import {m20260718h} from './m20260718h';
-import {m20260719} from './m20260719';
-import {m20260722} from './m20260722';
-import {m20260723} from './m20260723';
-import {m20260727} from './m20260727';
-import {m20260727b} from './m20260727b';
-import {m20260731} from './m20260731';
-import {m20260803} from './m20260803';
-import {m20260813} from './m20260813';
-import {m20260814} from './m20260814';
-import {m20260815} from './m20260815';
-import {m20260817} from './m20260817';
-import {m20260818} from './m20260818';
-import {m20260819} from './m20260819';
-import {m20260821} from './m20260821';
-import {m20260823} from './m20260823';
-import {m20260828} from './m20260828';
-import {m20260902} from './m20260902';
-import {m20260903} from './m20260903';
-import {m20260904} from './m20260904';
-import {m20260908} from './m20260908';
-import {m20260909} from './m20260909';
+import {m202606} from './m202606';
+import {m202607} from './m202607';
+import {m202608} from './m202608';
+import {m202609} from './m202609';
 
 export const migrationsRegistry: Record<string, string[]> = {
-	m20260619,
-	m20260620,
-	m20260622,
-	m20260703,
-	m20260703b,
-	m20260703c,
-	m20260705,
-	m20260705c,
-	m20260706,
-	m20260707,
-	m20260708,
-	m20260709,
-	m20260710,
-	m20260710b,
-	m20260710bc,
-	m20260710d,
-	m20260710e,
-	m20260714f,
-	m20260716,
-	m20260717,
-	m20260718,
-	m20260718a,
-	m20260718b,
-	m20260718c,
-	m20260718d,
-	m20260718e,
-	m20260718f,
-	m20260718g,
-	m20260718h,
-	m20260719,
-	m20260722,
-	m20260723,
-	m20260727,
-	m20260727b,
-	m20260731,
-	m20260803,
-	m20260813,
-	m20260814,
-	m20260815,
-	m20260817,
-	m20260818,
-	m20260819,
-	m20260821,
-	m20260823,
-	m20260828,
-	m20260902,
-	m20260903,
-	m20260904,
-	m20260908,
-	m20260909,
+	...m202606,
+	...m202607,
+	...m202608,
+	...m202609,
 };
---
+++ apps/migrate/src/migrations/m202609/index.ts
@@ -0,0 +1,13 @@
+import {m20260902} from './m20260902';
+import {m20260903} from './m20260903';
+import {m20260904} from './m20260904';
+import {m20260908} from './m20260908';
+import {m20260909} from './m20260909';
+
+export const m202609: Record<string, string[]> = {
+	m20260902,
+	m20260903,
+	m20260904,
+	m20260908,
+	m20260909,
+};
