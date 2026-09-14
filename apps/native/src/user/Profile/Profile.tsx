ab37add7f5856381e90abea911c96d8f9e30171e
Shayne Thiessen <shayne@shaynethiessen.com>
2026-09-14 10:39:41 -0500

    feat: timeline tab

--- apps/native/src/user/Profile/Profile.tsx
+++ apps/native/src/user/Profile/Profile.tsx
@@ -4,29 +4,34 @@ import React from 'react';
 import styles from '~/user/styles';
 import {useAuthentication} from '~/user/state/authentication/useAuthentication';
 import {ProfileDetails} from '~/user/Profile/components/ProfileDetails';
+import {ProfileTimeline} from '~/user/Profile/components/ProfileTimeline';
 import {Subscription} from '~/user/Profile/components/Subscription';
 import {Security} from '~/user/Profile/components/Security';
 import {Other} from '~/user/Profile/components/Other';
 
 type RouteParams = {
 	username: string;
-	tab?: 'details' | 'subscription' | 'security' | 'other';
+	tab?: 'timeline' | 'details' | 'subscription' | 'security' | 'other';
 };
 
 export function Profile() {
 	const authentication = useAuthentication();
 	const router = useRouter();
-	const {username, tab = 'details'} = useLocalSearchParams<RouteParams>();
+	const {username, tab = 'timeline'} = useLocalSearchParams<RouteParams>();
 
 	if (!authentication) return <Redirect href="/profile/login" />;
 
-	const switchTab = (newTab: 'details' | 'subscription' | 'security' | 'other') => {
+	const switchTab = (newTab: 'timeline' | 'details' | 'subscription' | 'security' | 'other') => {
 		router.setParams({username, tab: newTab});
 	};
 
 	return (
 		<View style={styles.container}>
 			<View style={{flexDirection: 'row', marginBottom: 20}}>
+				<TouchableOpacity style={[styles.tabButton, tab === 'timeline' && styles.activeTabButton]} onPress={() => switchTab('timeline')}>
+					<Text style={tab === 'timeline' ? styles.activeTabText : styles.tabText}>Timeline</Text>
+				</TouchableOpacity>
+
 				<TouchableOpacity style={[styles.tabButton, tab === 'details' && styles.activeTabButton]} onPress={() => switchTab('details')}>
 					<Text style={tab === 'details' ? styles.activeTabText : styles.tabText}>Details</Text>
 				</TouchableOpacity>
@@ -47,6 +52,7 @@ export function Profile() {
 				</TouchableOpacity>
 			</View>
 
+			{tab === 'timeline' && <ProfileTimeline username={username} />}
 			{tab === 'details' && <ProfileDetails username={username} />}
 			{tab === 'subscription' && <Subscription username={username} />}
 			{tab === 'security' && <Security username={username} />}
---
+++ apps/native/src/user/Profile/components/ProfileTimeline.tsx
@@ -0,0 +1,44 @@
+import {View, Text, Pressable} from 'react-native';
+import {Link} from 'expo-router';
+import React from 'react';
+import {formatDate, Spinner} from '@northernexplorer/tools-web';
+import styles from '~/user/styles';
+import {useApiFetch} from '~/core/useApiFetch';
+
+type Props = {
+	username: string;
+};
+
+export function ProfileTimeline({username}: Props) {
+	const {data, loading} = useApiFetch('user', 'UserController', 'getByUsername', {
+		username,
+	});
+	if (loading || !data) return <Spinner />;
+
+	const ProfileField = ({label, value}: {label: string; value: string | number}) => (
+		<View style={styles.field}>
+			<Text style={styles.label}>{label}</Text>
+			<Text style={styles.value}>{value}</Text>
+		</View>
+	);
+
+	return (
+		<View style={styles.container}>
+			<ProfileField label="First Name" value={data.firstName} />
+			<ProfileField label="Last Name" value={data.lastName} />
+			<ProfileField label="Username" value={data.username} />
+			<ProfileField label="Gender" value={data.gender} />
+			<ProfileField label="Birthday" value={formatDate(data.birthday)} />
+			<ProfileField label="Email Address" value={data.email} />
+			<ProfileField label="Score" value={data.score} />
+			<ProfileField label="Status" value={data.isActive ? 'Active' : 'Inactive'} />
+
+			<ProfileField label="Registered On" value={formatDate(data.createdAt)} />
+			<Link href={`/profile/${username}/edit-profile`} asChild>
+				<Pressable style={styles.button}>
+					<Text style={styles.buttonText}>Edit Profile</Text>
+				</Pressable>
+			</Link>
+		</View>
+	);
+}
