d1a0d3ff8e15a155faee2283934e968e77fd1ed0
Shayne Thiessen <shayne@shaynethiessen.com>
2026-09-17 15:12:47 -0500

    feat: expo-share-intent

--- apps/native/src/app/_layout.tsx
+++ apps/native/src/app/_layout.tsx
@@ -1,6 +1,7 @@
 import {Stack} from 'expo-router';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
+import {ShareIntentProvider} from 'expo-share-intent';
 import {store, persistor} from '~/core/store';
 import {AppBootstrap} from '~/layout/Layout/components/Boostrap';
 import {ConnectivityProvider} from '~/core/ConnectivityProvider';
@@ -8,19 +9,21 @@ import {AlertHandler} from '~/layout/Layout/components/AlertHandler';
 
 export default function Layout() {
 	return (
-		<Provider store={store}>
-			<ConnectivityProvider>
-				<PersistGate loading={null} persistor={persistor}>
-					<AlertHandler>
-						<AppBootstrap />
-						<Stack
-							screenOptions={{
-								headerShown: false,
-							}}
-						/>
-					</AlertHandler>
-				</PersistGate>
-			</ConnectivityProvider>
-		</Provider>
+		<ShareIntentProvider>
+			<Provider store={store}>
+				<ConnectivityProvider>
+					<PersistGate loading={null} persistor={persistor}>
+						<AlertHandler>
+							<AppBootstrap />
+							<Stack
+								screenOptions={{
+									headerShown: false,
+								}}
+							/>
+						</AlertHandler>
+					</PersistGate>
+				</ConnectivityProvider>
+			</Provider>
+		</ShareIntentProvider>
 	);
 }
